689bc69bb36a01c89ba571160f941d8bd59d7d60
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2021-11-08 10:08:41 +0530

    Added docstrings and blacked code.

--- db.py
+++ db.py
@@ -4,17 +4,31 @@ from uuid import UUID
 from datetime import datetime
 
 
-async def returnServerJson(usern: str):
+async def returnUserJson(usern: str) -> dict:
+    """Returns a dict containing all instances of the user, predicted future playing times, and more.
+
+    Args:
+        usern (str): The user to get data for.
+
+    Returns:
+        dict: A dict containing all instances of the user, predicted future playing times, and more.
+    """
+
     req = get("https://api.mojang.com/users/profiles/minecraft/" + usern).json()
     id = str(UUID(req["id"]))
+    """Gets the UUID of the user."""
+
     conn = await aiosqlite.connect("players.db")
     conn.row_factory = aiosqlite.Row
     found = await conn.execute(f"SELECT * FROM players WHERE ID LIKE '{id}'")
     found = list(await found.fetchall())
+    """Pulls all instances of the user from the database, asynchroneously."""
 
     info = {
         "userinfo": {"name": req["name"], "id": id},
+        """Returns the user's username, as per proper case and their uuid with dashes."""
         "servers": list(set([server["IP"] for server in [dict(x) for x in found]])),
+        """Returns a unique list of all servers the user has played on."""
         "sorted": [
             dict(sorted(x.items(), key=lambda x: x[1]))
             for x in [
@@ -22,6 +36,7 @@ async def returnServerJson(usern: str):
                 for server in [dict(x) for x in found]
             ]
         ],
+        """Returns a list of dicts, each containing a server and the time the user last played on that server, all sorted chronologically."""
         "utctime": [
             {
                 server["IP"]: datetime.utcfromtimestamp(
@@ -30,13 +45,24 @@ async def returnServerJson(usern: str):
             }
             for server in [dict(x) for x in found]
         ],
+        """Returns a list of dicts, each containing a server and the time the user last played on that server, chronologically sorted, but with the dates in UTC instead of UNIX."""
         "times": [server["TIMESTAMP"] for server in [dict(x) for x in found]],
+        """Returns a list of all the times the user has played on a server (Useful for predicting the next time a user will log on)."""
+        "success": True,  # Just added this to make sure the function doesn't break.
     }
     await conn.close()
     return info
 
 
-def avg(dates):
+def avg(dates) -> str:
+    """Returns the average of a list of dates.
+
+    Args:
+        dates (datetime objects): A list of datetime objects.
+
+    Returns:
+        string: The average of the list of dates.
+    """
     any_reference_date = datetime(1900, 1, 1)
     x = any_reference_date + sum(
         [date - any_reference_date for date in dates], datetime.timedelta()
