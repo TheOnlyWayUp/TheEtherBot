761add7d4a22f6dd9decddce02509af35b667fcb
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2021-11-10 11:10:53 +0530

    Added hostname, server commands

---
+++ dbFunctions.py
@@ -0,0 +1,134 @@
+import aiosqlite, datetime, requests
+from requests import get
+from uuid import UUID
+from mctools import PINGClient
+
+async def returnUserJson(usern: str) -> dict:
+    """Returns a dict containing all instances of the user, predicted future playing times, and more.
+
+    Args:
+        usern (str): The user to get data for.
+
+    Returns:
+        dict: A dict containing all instances of the user, predicted future playing times, and more.
+    """
+
+    req = get("https://api.mojang.com/users/profiles/minecraft/" + usern).json()
+    id = str(UUID(req["id"]))
+    """Gets the UUID of the user."""
+
+    PlayerCon = await aiosqlite.connect("players.db")
+    PlayerCon.row_factory = aiosqlite.Row
+    found = await PlayerCon.execute(f"SELECT * FROM players WHERE ID LIKE '{id}'")
+    found = list(await found.fetchall())
+    """Pulls all instances of the user from the database, asynchroneously."""
+
+    info = {
+        "userinfo": {"name": req["name"], "id": id},
+        "servers": list(set([server["IP"] for server in [dict(x) for x in found]])),
+        "sorted": [
+            dict(sorted(x.items(), key=lambda x: x[1]))
+            for x in [
+                {server["IP"]: server["TIMESTAMP"]}
+                for server in [dict(x) for x in found]
+            ]
+        ],
+        "utctime": [
+            {
+                server["IP"]: datetime.datetime.utcfromtimestamp(
+                    int(server["TIMESTAMP"] / 1000)
+                ).strftime("%Y-%m-%d %H:%M:%S")
+            }
+            for server in [dict(x) for x in found]
+        ],
+        "times": [server["TIMESTAMP"] for server in [dict(x) for x in found]],
+        "success": True,  # Just added this to make sure the function doesn't break.
+    }
+    """Returns the user's username, as per proper case and their uuid with dashes."""
+    """Returns a unique list of all servers the user has played on."""
+    """Returns a list of dicts, each containing a server and the time the user last played on that server, all sorted chronologically."""
+    """Returns a list of dicts, each containing a server and the time the user last played on that server, chronologically sorted, but with the dates in UTC instead of UNIX."""
+    """Returns a list of all the times the user has played on a server (Useful for predicting the next time a user will log on)."""
+    await PlayerCon.close()
+    return info
+
+
+def avg(dates) -> str:
+    """Returns the average of a list of dates.
+
+    Args:
+        dates (datetime objects): A list of datetime objects.
+
+    Returns:
+        string: The average of the list of dates.
+    """
+    any_reference_date = datetime.datetime(1900, 1, 1)
+    x = any_reference_date + sum(
+        [date - any_reference_date for date in dates], datetime.timedelta()
+    ) / len(dates)
+    return x.strftime("%H:%M:%S")
+
+
+
+async def returnHostname(server: str) -> str:
+    """Returns the hostname of a server.
+
+    Args:
+        server (str): The server to get data for.
+    """
+    DNSCon = await aiosqlite.connect("dns.db")
+    DNSCon.row_factory = aiosqlite.Row
+    try:
+        found = await DNSCon.execute(f"SELECT * FROM DNS_TABLE WHERE ip LIKE '{server}' ORDER BY timestamp DESC LIMIT 1")
+        found = list(await found.fetchall())
+        found = [dict(info) for info in found][0]
+        print(found)
+        return found['DNS']
+    except Exception as e:
+        print(e)
+        return None
+    await DNSCon.close()
+    
+async def returnServerJson(server: str) -> dict:
+    """Returns a dict containing basic server information.
+
+    Args:
+        server (str): The server to get data for.
+
+    Returns:
+        dict: A dict containing basic server information.
+    """
+
+    ServerCon = await aiosqlite.connect("videlicet.db")
+    ServerCon.row_factory = aiosqlite.Row
+    found = await ServerCon.execute(f"SELECT * FROM BASIC_PINGS WHERE ip LIKE '{server}' ORDER BY timestamp DESC LIMIT 1")
+    found = list(await found.fetchall())
+    found = [dict(info) for info in found][0]
+    print(found)
+    info = {"ip":{"ip":found['IP'], "port":found['PORT'], "hostname":await returnHostname(server) if await returnHostname(server) else found['IP']}, "version":found['VERSION'], "players":{"online":found['PLAYERS_ONLINE'], "max":found['MAX_PLAYERS']}, "motd":found['MOTD'], "success":True}
+    await ServerCon.close()
+    return info
+    
+def why(a, *, shit_to_do):
+    try:
+        return exec(shit_to_do)
+    except:
+        return None
+async def returnPingJson(server: str) -> dict:
+    ping = PINGClient(host=server, timeout=5)
+    ping.stop()
+    stats = ping.get_stats()
+    try:
+        motd = "".join([motd["text"] for motd in requests.get(f"https://eu.mc-api.net/v3/server/ping/{server}").json()["description"]["extra"]])
+    except KeyError:
+        motd = "No MOTD."
+    except:
+        motd = requests.get(f"https://eu.mc-api.net/v3/server/ping/{server}").json()["description"]["text"]
+    info = {"ip":{"ip":server, "port":"25565", "hostname":await returnHostname(server) if await returnHostname(server) else server}, "version":stats['version']['name'] if stats['version']['name'] else None, "players":{"online":stats['players']['online'], "max":stats['players']['max']}, "motd":motd, "success":True}
+    if stats['players']['online'] != 0:
+        info['players']['players'] = "\n".join(stats['players']['sample'])
+    else:
+        info['players']['players'] = "No one online."
+    return info
+    
+    
--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-import discord, db, datetime
+import discord, dbFunctions, datetime
 from discord.ext import commands
 
 # from discord_slash import SlashCommand, SlashContext
@@ -25,7 +25,7 @@ async def on_ready():
 )
 async def player(ctx, username):
     try:
-        info = await db.returnUserJson(username)
+        info = await dbFunctions.returnUserJson(username)
         embedList = []
         em = discord.Embed(
             title=info["userinfo"]["name"],
@@ -75,5 +75,33 @@ async def player(ctx, username):
     except:
         await ctx.respond("User not found.")
 
+
+@bot.command(help="Finds the hostname, or tries to.")
+async def hostname(ctx, *, ip):
+    hostname = await dbFunctions.returnHostname(ip)
+    if hostname is None:
+        await ctx.send("Couldn't find a hostname in the db.")
+    else:
+        await ctx.send(hostname)
+    
+@bot.command(help="Return information about a server from the Database.")
+async def server(ctx, *, ip):
+    info = await dbFunctions.returnServerJson(ip)
+    print(info)
+    e = discord.Embed(title=f"{info['ip']['hostname']}:{info['ip']['port']}", description="If you're unable to find information here, try pinging the server instead.", color=0x759851)
+    e.add_field(name="MOTD", value=info['motd'], inline=False)
+    e.add_field(name="Players", value=f"{info['players']['online']}/{info['players']['max']}", inline=False)
+    e.add_field(name="Version", value=info['version'], inline=False)
+    await ctx.send(embed=e)
+
+@bot.command(help="Ping a server and show current information about a server.")
+async def ping(ctx, *, ip):
+    info = await dbFunctions.returnPingJson(ip)
+    print(info)
+    e = discord.Embed(title=f"{info['ip']['hostname']}:{info['ip']['port']}", description="Server Pinged.", color=0x759851)
+    e.add_field(name="MOTD", value=info['motd'], inline=False)
+    e.add_field(name="Players", value=f"{info['players']['online']}/{info['players']['max']}"+"\n"+info['players']['players'], inline=False)
+    e.add_field(name="Version", value=info['version'], inline=False)
+    await ctx.send(embed=e)
 
 bot.run("OTA0Njg1NjI3NzA5MjA2NTQ5.YX_IJQ.dImIiqyH8QKq-auHbueNfxmeXA4")
